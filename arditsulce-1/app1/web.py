f06e02299cbb5049d646c9825d9e719c9c990aab
sambak73 <raghuisnow@yahoo.com>
2023-01-18 02:50:50 +0530

    removed many write statement

--- arditsulce-1/app1/web.py
+++ arditsulce-1/app1/web.py
@@ -5,31 +5,18 @@ st.write("This app is to increase your productivity")
 
 todos = functions.get_todos()
 
-#st.session_state.todo = False
-#st.write(st.session_state)
-
 def add_todo():
-    #st.write(st.session_state['todo'])
-    #st.write(todo)
     new_todo = st.session_state.todo + "\n"
-    #st.write(new_todo)
     todos.append(new_todo)
-    #st.write(todos)
     functions.write_todos(todos)
     
 def get_selected_todo_checkbox():
     for i in st.session_state.keys():
         if (i.startswith('todo_checkbox_') and st.session_state[i]):
             selected = i.replace('todo_checkbox_','')
-            #st.text_input("todo") = selected
-            #selected = selected.strip('\n')
-            #todo = st.text_input("ToDo to add to list",value=selected,placeholder="Enter todo here...",label_visibility="visible",key="edit-todo")    
-            #st.write(selected)
             st.write(todos[int(selected)])
             
 for index,todo in enumerate(todos):
-    #st.experimental_rerun()
-    #st.checkbox(f"{index+1}.{todo}",on_change=get_selected_todo_checkbox,key='todo_checkbox_' + str(index))
     checkbox = st.checkbox(todo,key=todo)
     if checkbox:
         todos.pop(index)
